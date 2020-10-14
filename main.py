a5e107708c262fdbf97b09355149bb8bdf92b7fc
jamielaird <jamielaird@gmail.com>
2020-10-14 21:44:46 +0100

    added zones 2-4

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import config
 import requests
 import json
 import datetime
-# from datetime import date, timedelta
 
 # get bearer token
 data = {
@@ -27,13 +26,43 @@ homeId = res['homeId']
 url = 'https://my.tado.com/api/v2/homes/'+str(homeId)+'/zones'
 raw = requests.get(url, headers=headers)
 res = raw.json()
-zone = '1'
 
-# get historical data 
+# get historical data for zone 1
+zone = '1'
 params = (('date', str(datetime.date.today() - datetime.timedelta(days=1))),)
 raw = requests.get('https://my.tado.com/api/v2/homes/'+str(homeId)+'/zones/'+zone+'/dayReport', headers=headers, params=params)
 res = raw.json()
 
-# parse timestamp and temp
+# parse timestamp and temp for zone 1
+for res5 in res['measuredData']['insideTemperature']['dataPoints']:
+    print(zone,res5['timestamp'], res5['value']['celsius'])
+
+# get historical data for zone 2
+zone = '2'
+params = (('date', str(datetime.date.today() - datetime.timedelta(days=1))),)
+raw = requests.get('https://my.tado.com/api/v2/homes/'+str(homeId)+'/zones/'+zone+'/dayReport', headers=headers, params=params)
+res = raw.json()
+
+# parse timestamp and temp for zone 2
+for res5 in res['measuredData']['insideTemperature']['dataPoints']:
+    print(zone,res5['timestamp'], res5['value']['celsius'])
+
+# get historical data for zone 3
+zone = '3'
+params = (('date', str(datetime.date.today() - datetime.timedelta(days=1))),)
+raw = requests.get('https://my.tado.com/api/v2/homes/'+str(homeId)+'/zones/'+zone+'/dayReport', headers=headers, params=params)
+res = raw.json()
+
+# parse timestamp and temp for zone 3
+for res5 in res['measuredData']['insideTemperature']['dataPoints']:
+    print(zone,res5['timestamp'], res5['value']['celsius'])
+
+# get historical data for zone 4
+zone = '4'
+params = (('date', str(datetime.date.today() - datetime.timedelta(days=1))),)
+raw = requests.get('https://my.tado.com/api/v2/homes/'+str(homeId)+'/zones/'+zone+'/dayReport', headers=headers, params=params)
+res = raw.json()
+
+# parse timestamp and temp for zone 4
 for res5 in res['measuredData']['insideTemperature']['dataPoints']:
     print(zone,res5['timestamp'], res5['value']['celsius'])
