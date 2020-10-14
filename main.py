6191ca74d5d049d13aeee477994dcbe490134f4c
jamielaird <jamielaird@gmail.com>
2020-10-15 00:12:29 +0100

    write results for zone 1 to postgres

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import config
 import requests
 import json
 import datetime
+import psycopg2
 
 # get bearer token
 data = {
@@ -33,36 +34,16 @@ params = (('date', str(datetime.date.today() - datetime.timedelta(days=1))),)
 raw = requests.get('https://my.tado.com/api/v2/homes/'+str(homeId)+'/zones/'+zone+'/dayReport', headers=headers, params=params)
 res = raw.json()
 
-# parse timestamp and temp for zone 1
+# open database connection
+conn = psycopg2.connect('dbname=tado')
+cursor = conn.cursor()
+
+# iterate through results to parse zone, timestamp and temperature and write to postgres (tado.inside_temperature)
 for res5 in res['measuredData']['insideTemperature']['dataPoints']:
-    print(zone,res5['timestamp'], res5['value']['celsius'])
+    cursor.execute("INSERT INTO inside_temperature (zone, timestamp, temperature) VALUES (%s, %s, %s)",(int(zone),str(res5['timestamp']),float(res5['value']['celsius'])))
 
-# get historical data for zone 2
-zone = '2'
-params = (('date', str(datetime.date.today() - datetime.timedelta(days=1))),)
-raw = requests.get('https://my.tado.com/api/v2/homes/'+str(homeId)+'/zones/'+zone+'/dayReport', headers=headers, params=params)
-res = raw.json()
+# commit and close database connection
+conn.commit()
+cursor.close()
+conn.close()
 
-# parse timestamp and temp for zone 2
-for res5 in res['measuredData']['insideTemperature']['dataPoints']:
-    print(zone,res5['timestamp'], res5['value']['celsius'])
-
-# get historical data for zone 3
-zone = '3'
-params = (('date', str(datetime.date.today() - datetime.timedelta(days=1))),)
-raw = requests.get('https://my.tado.com/api/v2/homes/'+str(homeId)+'/zones/'+zone+'/dayReport', headers=headers, params=params)
-res = raw.json()
-
-# parse timestamp and temp for zone 3
-for res5 in res['measuredData']['insideTemperature']['dataPoints']:
-    print(zone,res5['timestamp'], res5['value']['celsius'])
-
-# get historical data for zone 4
-zone = '4'
-params = (('date', str(datetime.date.today() - datetime.timedelta(days=1))),)
-raw = requests.get('https://my.tado.com/api/v2/homes/'+str(homeId)+'/zones/'+zone+'/dayReport', headers=headers, params=params)
-res = raw.json()
-
-# parse timestamp and temp for zone 4
-for res5 in res['measuredData']['insideTemperature']['dataPoints']:
-    print(zone,res5['timestamp'], res5['value']['celsius'])
